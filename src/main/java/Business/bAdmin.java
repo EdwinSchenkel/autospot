41e90deca677009ca5bb597a1c95793c3a7c28f2
Edwin Schenkel <E.Schenkel@macview.nl>
2019-10-29 11:32:08 +0100

    for the video

--- src/main/java/Business/bAdmin.java
+++ src/main/java/Business/bAdmin.java
@@ -4,45 +4,32 @@ import Helpers.DataConnection;
 import Logging.Logging;
 import Models.Users;
 
-public class bAdmin
-{
-    public void editUser(Users user)
-    {
-        try (var db = new DataConnection())
-        {
+public class bAdmin {
+    public void editUser(Users user) {
+        try (var db = new DataConnection()) {
             db.editObject(user.getClass(), user, user.getId());
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             Logging.HandleError(ex);
         }
     }
 
-    public boolean deleteUser(int id)
-    {
-        try (var db = new DataConnection())
-        {
+    public boolean deleteUser(int id) {
+        try (var db = new DataConnection()) {
             var item = db.getObjectFromQuery(new Users(), "SELECT u FROM Users u WHERE Id = " + id);
             return db.deleteItem(item);
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             Logging.HandleError(ex);
         }
 
         return false;
     }
 
-    public boolean isAdmin(int id)
-    {
-        try(var db = new DataConnection())
-        {
+    public boolean isAdmin(int id) {
+        try (var db = new DataConnection()) {
             var admin = db.getObjectFromQuery(new Users(), "SELECT u FROM Users u WHERE Id = " + id);
-            if(admin != null)
+            if (admin != null)
                 return admin.isAdmin();
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             Logging.HandleError(ex);
         }
         return false;
